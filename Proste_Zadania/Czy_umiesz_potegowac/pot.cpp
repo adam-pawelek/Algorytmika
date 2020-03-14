e9d49593ead6f5035089fc970d297ae960c4a761
adam-pawelek <adam.pwk@outlook.com>
2020-03-15 00:12:48 +0100

    zrobione zadanie Czy umiesz potegowac

--- Proste_Zadania/Czy_umiesz_potegowac/pot.cpp
+++ Proste_Zadania/Czy_umiesz_potegowac/pot.cpp
@@ -1,13 +1,27 @@
 #include <iostream>
 using namespace std;
+long long tab[1000000];
 int main (){
-	int liczba;
-	int pot;
-	int wyn = 1;
+	long long liczba;
+	long long pot;
 	cin >>liczba>>pot;
-	for (int i = 0; i < pot; i++){
-		wyn = wyn * liczba;
-		cout <<wyn<<"\n"; 
+	long long licz = 0;
+	tab[licz] = liczba % 10;
+	long long  wyn;
+	wyn = (liczba * liczba)%10;
+	licz = 1;
+	tab[licz] = wyn;
+	while (liczba % 10 != wyn % 10){
+		wyn = (wyn * liczba) % 10;
+		licz++;
+		tab[licz] = wyn;
 	}
+	/*
+	for (int i = 0 ; i < licz; i++){
+		cout <<tab[i]<<"\n";
+	}
+	cout <<"licznik"<<licz<<"\n";	
+	*/
+	cout <<tab[(pot -1) % licz];
 	return 0;
 }
