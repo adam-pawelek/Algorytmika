13994b6a8d05138bd435f932963b450f4a3450bf
adam-pawelek <adam.pwk@outlook.com>
2020-03-16 19:36:18 +0100

    Dodanie zadania Masz_Orientacje

--- Proste_Zadania/Masz_Orientacje/Masz_Orientacje.cpp
+++ Proste_Zadania/Masz_Orientacje/Masz_Orientacje.cpp
@@ -1,8 +1,44 @@
 #include <iostream>
 
-using nmaespace std;
-
+using namespace std;
+int tab [3000 * 100];
 int main(){
+	ios_base::sync_with_stdio(0);
+	int lip; // liczba punktow
+	int licz = 1;
+	cin >>lip;
+	for (int i = 0; i < lip; i++){
+		cin >>tab[i];
+		tab[i+lip] = tab[i];
+	}
 
+	
+
+	for (int i = 0; i < 2 * lip; i++){
+		if (tab[i] >= tab[i+1]){
+			licz++;
+		}
+		else{
+			licz = 1;
+		}
+		if (licz == lip){
+			cout <<"TAK";
+			return 0;
+		}
+	}
+	licz = 1;
+	for (int i = 0; i < 2 * lip; i++){
+		if (tab[i] <= tab[i+1]){
+			licz++;
+		}
+		else{
+			licz = 1;
+		}
+		if (licz == lip){
+			cout <<"TAK";
+			return 0;
+		}
+	}
+	cout <<"NIE";
 	return 0;
 }
